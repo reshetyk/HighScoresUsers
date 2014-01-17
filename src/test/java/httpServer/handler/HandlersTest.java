fca6f88e042ccf506efed0f5565a718313c17321
Alexey <reshetyk@gmail.com>
2014-01-17 18:27:14 +0200

    - fix and add more checks to http service test

--- src/test/java/httpServer/handler/HandlersTest.java
+++ src/test/java/httpServer/handler/HandlersTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import static httpServer.handler.HttpUrlConnector.RequestMethod.GET;
 import static httpServer.handler.HttpUrlConnector.RequestMethod.POST;
 import static httpServer.handler.HttpUrlConnector.doHttpRequest;
+import static junit.framework.Assert.*;
 
 /**
  * @author Alexey
@@ -19,25 +20,52 @@ public class HandlersTest {
 
     @BeforeClass
     public void setUp() throws Exception {
+        //start the server
         Launcher.main(new String[]{});
     }
 
     @Test
     public void testAddUserIdLevelScoreHandler() throws Exception {
+        doHttpRequest(SERVICE_HOST_URL + "/postscore?user=142&level=12&score=5661", POST);
+        doHttpRequest(SERVICE_HOST_URL + "/postscore?user=142&level=10&score=6000", POST);
+        doHttpRequest(SERVICE_HOST_URL + "/postscore?user=142&level=5&score=7888", POST);
+        doHttpRequest(SERVICE_HOST_URL + "/postscore?user=150&level=5&score=7500", POST);
+        doHttpRequest(SERVICE_HOST_URL + "/postscore?user=150&level=10&score=6555", POST);
         String result = doHttpRequest(SERVICE_HOST_URL + "/postscore?user=142&level=12&score=5661", POST);
-        Assert.assertTrue(result.isEmpty());
+        assertTrue(result.isEmpty());
     }
 
     @Test(dependsOnMethods = "testAddUserIdLevelScoreHandler")
     public void testGetLevelScoresByUserIdHandler() throws Exception {
-        String result = doHttpRequest(SERVICE_HOST_URL + "/userscores?userid=142", GET);
-        Assert.assertEquals("12=5661", result);
+
+        assertEquals(
+                "12=5661;10=6000;5=7888;5=7500",
+                doHttpRequest(SERVICE_HOST_URL + "/userscores?userid=142", GET)
+        );
+
+        assertEquals(
+                "5=7500;10=6555",
+                doHttpRequest(SERVICE_HOST_URL + "/userscores?userid=150", GET)
+        );
     }
 
     @Test(dependsOnMethods = "testAddUserIdLevelScoreHandler")
     public void testGetUserIdScoreByLevelHandler() throws Exception {
-        String result = doHttpRequest(SERVICE_HOST_URL + "/levelscores?user=142&level=12&score=5661", GET);
-        Assert.assertEquals("142=5661", result);
+
+        assertEquals(
+                "142=5661",
+                doHttpRequest(SERVICE_HOST_URL + "/levelscores?level=12", GET)
+        );
+
+        assertEquals(
+                "142=6000;150=6555",
+                doHttpRequest(SERVICE_HOST_URL + "/levelscores?level=10", GET)
+        );
+
+        assertEquals(
+                "142=7888;150=7500",
+                doHttpRequest(SERVICE_HOST_URL + "/levelscores?level=5", GET)
+        );
     }
 }
 
